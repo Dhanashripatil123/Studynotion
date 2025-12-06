902301ddf23f392ec6747caf9257391118c812e4
dhanashri <bdhffkfkv@gamal.com>
2025-12-06 10:03:00 +0530

    Restore from .history (~11h ago) - finalize

--- studynotion/src/components/core/catalog/CourseSlider.jsx
+++ studynotion/src/components/core/catalog/CourseSlider.jsx
@@ -14,23 +14,23 @@ const CourseSlider = ({courses, Courses})=>{
                    <>
                          {
                                list?.length ? (
-                              <Swiper
-                                    slidesPerView={1}
-                                    spaceBetween={10}
-                                    loop={true}
-                                    modules={[Pagination, A11y, Navigation, Scrollbar]}
-                                    navigation
-                                    pagination={{ clickable: true }}
-                                    scrollbar={{ draggable: true }}
-                                    onSwiper={(swiper) => console.log('swiper init', swiper)}
-                                    onSlideChange={() => console.log('slide change')}
-                              >
-                                    {list.map((course, index) => (
-                                          <SwiperSlide key={course.courseId || course._id || index}>
-                                                <Course_Card course={course} Height={"h-[250px]"} />
-                                          </SwiperSlide>
-                                    ))}
-                              </Swiper>
+                                                <Swiper
+                                                      slidesPerView={1}
+                                                      spaceBetween={10}
+                                                      loop={true}
+                                                      modules={[Pagination, A11y, Navigation, Scrollbar]}
+                                                      navigation
+                                                      pagination={{ clickable: true }}
+                                                      scrollbar={{ draggable: true }}
+                                                      onSwiper={(swiper) => console.log('swiper init', swiper)}
+                                                      onSlideChange={() => console.log('slide change')}
+                                                >
+                                                      {list.map((course, index) => (
+                                                            <SwiperSlide key={course.courseId || course._id || index}>
+                                                                  <Course_Card course={course} Height={"h-[250px]"} />
+                                                            </SwiperSlide>
+                                                      ))}
+                                                </Swiper>
                         ) : (
                    <p>No Course Found</p>                               
                 )
