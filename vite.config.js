3fbec3dbd08ab1d0494da1ad595d39a817a10357
dhanashri <bdhffkfkv@gamal.com>
2026-02-11 15:04:39 +0530

    Fix Vite base path and proxy for Render

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,13 @@ export default defineConfig({
   plugins: [react(),  tailwindcss()],
   server: {
     port: 3000, 
+      proxy: {
+      "/api": {
+        target: "http://localhost:5000", // your backend port
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
   base: "/",
   
